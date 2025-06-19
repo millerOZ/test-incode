ceb4ba2bd7530ffd4ab6c90aa7d61394cf96e4ce
miller <miller.ossa@sofka.com.co>
2025-06-19 08:30:58 -0500

    configuracion app.config, angular json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withEnabledBlockingInitialNavigation, withHashLocation } from '@angular/router';
-import { routes } from './app.routes';
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
-  ],
+  providers: [provideRouter(routes)],
 };
